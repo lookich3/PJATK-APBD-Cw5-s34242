023fbfd0458748759f0329fd779829b39fdf723c
Oleksii Nyskoroda <s34242@pjwtsk.edu.pl>
2026-04-14 18:14:39 +0200

    Dodalem modele do projektu

---
+++ WebApplication3/Models/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models;
+
+public class Reservation : IValidatableObject
+{
+    public int Id { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int RoomId { get; set; }
+
+    [Required]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required]
+    public string Topic { get; set; } = string.Empty;
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    [Required]
+    public string Status { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+        {
+            yield return new ValidationResult(
+                "EndTime musi być póżniejszy niż StartTime",
+                new[] { nameof(EndTime), nameof(StartTime) });
+        }
+    }
+}
